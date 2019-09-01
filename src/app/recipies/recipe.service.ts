4a6d29b5f33641619476df0504b54ab94f0f2893
Dmitry Mamonov <dmitry.mamonov@corp.mail.ru>
2019-09-01 18:07:36 +0300

    add ingredients to the recipes

--- src/app/recipies/recipe.service.ts
+++ src/app/recipies/recipe.service.ts
@@ -1,13 +1,31 @@
 import { Recipe } from './recipe.model';
 import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
   private recipies: Recipe[] = [
-    new Recipe('test recipe', 'test description', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Vegetarian-Pad-Thai_EXPS_HCK18_197935_B04_014_4b-1-696x696.jpg'),
-    new Recipe('new test recipe', 'test description', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Roasted-Peppers-and-Cauliflower_EXPS_SDDJ19_30434_E07_20_7b-1024x1024.jpg'),
+    new Recipe(
+      'test recipe',
+      'test description',
+      'https://www.tasteofhome.com/wp-content/uploads/2018/01/Vegetarian-Pad-Thai_EXPS_HCK18_197935_B04_014_4b-1-696x696.jpg',
+      [
+        new Ingredient('test ingredient', 2),
+        new Ingredient('another ingredient', 3),
+        new Ingredient('zzz', 100),
+      ],
+    ),
+    new Recipe(
+      'new test recipe',
+      'test description',
+      'https://www.tasteofhome.com/wp-content/uploads/2018/01/Roasted-Peppers-and-Cauliflower_EXPS_SDDJ19_30434_E07_20_7b-1024x1024.jpg',
+      [
+        new Ingredient('ingredient', 5),
+        new Ingredient('another ingredient', 1),
+      ],
+    ),
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
